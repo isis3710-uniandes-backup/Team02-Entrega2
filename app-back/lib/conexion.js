177a856d0837f65b9f92d2926bdc2b58b46b7ad4
Mateo Sicard <sicard6@gmail.com>
2019-09-19 22:39:17 -0500

    little comment

--- app-back/lib/conexion.js
+++ app-back/lib/conexion.js
@@ -4,6 +4,7 @@ herramienta de modelado
 let mongoClient = require('mongodb').MongoClient;
 
 /*
+*Be aware. change local host at the moment of deployment
 Obtiene el componente de mongo
 */
 var conn = mongoClient.connect('mongodb://localhost:27017', {
