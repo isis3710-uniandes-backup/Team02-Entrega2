eee4a1a4ee9459be5f2ec802d55c36954b854533
Geovanny Andrés González <ga.gonzalezr@uniandes.edu.co>
2019-09-22 00:59:23 -0500

    Listo el html y el funcionamiento de las vistas, toca verificar que la peticion del backend sea correcta

--- app-back/app-front/src/components/Auth/Login.js
+++ app-back/app-front/src/components/Auth/Login.js
@@ -7,7 +7,7 @@ const url = "http://localhost:3001";
 class Login extends Component {
 	//TODO Login con back
 	//TODO Verificar la información con el backend y verificar que sea veridica.
-	//TODO Actualizar el nombre de usuario con el obtenido		
+	//TODO Actualizar el nombre de usuario con el obtenido.		
 
 	/**
 	 * * Las variables del estado son: uemail - Correo del usuario y upass - Contraseña digitada.
@@ -22,8 +22,9 @@ class Login extends Component {
 		event.preventDefault();
 		console.log(`El Email ingresado fue: ${this.state.uemail} y el pass fue: ${this.state.upass}`);
 
-		//* Llamar al backend.
-		fetch(url).then((res) => res.json()).then((data) => {
+		//* Llamar al backend: Peticion traer los datos de inicio de sesión.
+
+		fetch(url + `/users/${this.state.uemail}/${this.state.upass}`).then((res) => res.json()).then((data) => {
 			if (data.length === 0) return alert("The email and password doesn't match with any registered user, check the credentials");
 			let user_data = data[0]; //* El backend retorna el objeto sobre un arreglo.
 			this.props.userf(user_data.userName); //* El atributo userName contiene el nombre del usuario, cambiar el nombre del usuario en la app.
@@ -42,11 +43,11 @@ class Login extends Component {
 
 	render() {
 		return (
-			<div className="col-4 justify-content-center">
+			<div className="col-4 justify-content-center centrar">
 				<div className="container-fluid justify-content-center">
 					<h5 className="loginTitle">Login</h5>
 				</div>
-				<form onSubmit={this.clickLog} className="titleFormSpace">
+				<form className="titleFormSpace">
 					<div className="form-group">
 						<label>User's Email</label>
 						<input type="email" name="uEmail" className="form-control" id="userEmail" aria-describedby="emailHelp" placeholder="Enter your user's email" onChange={(evt) => this.setState({uemail: evt.target.value})} required/>						
@@ -56,7 +57,7 @@ class Login extends Component {
 						<input type="password" name="uPass" className="form-control" id="userPass" placeholder="User's account password" onChange={(evt) => this.setState({upass: evt.target.value})} required/>
 					</div>		
 					<div className="row button-row">
-						<button type="submit" className="btn btn-success logButtonSpace"> Log In </button>					
+						<button type="button" onClick={this.clickLog} className="btn btn-success logButtonSpace"> Log In </button>					
 						<button type="button" onClick={this.clickSignIn} className="btn btn-secondary">Sign In</button>
 					</div>
 				</form>
